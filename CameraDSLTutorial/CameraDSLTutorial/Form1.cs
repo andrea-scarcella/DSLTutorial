ca1446bf3479aec0721953ab100598c2b3bb5ece
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2012-12-09 16:11:59 +0100

    Added project from first half of tutorial

---
+++ CameraDSLTutorial/CameraDSLTutorial/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Irony.Parsing;
+
+namespace CameraDSLTutorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string programText = textBox1.Text;
+            CameraControlGrammar grammar = new CameraControlGrammar();
+            Parser parser = new Parser(grammar);
+            ParseTree program = parser.Parse(programText);
+           
+            //var ttt=program.ToXml();
+
+
+
+            var cameraSizeNode = program.Root.ChildNodes[0];
+            var widthNode = cameraSizeNode.ChildNodes[0];
+            int width = (int)widthNode.Token.Value;
+            var heightNode = cameraSizeNode.ChildNodes[1];
+            int height = (int)heightNode.Token.Value;
+
+
+
+            // loop through the movement commands
+            foreach (ParseTreeNode commandNode in program.Root.ChildNodes[2].ChildNodes)
+            {
+                // get the number of pixels to move
+                Token pixelsToken = commandNode.ChildNodes[0].Token;
+                int pixelsToMove = (int)pixelsToken.Value;
+                // get the direction
+                Token directionToken =commandNode.ChildNodes[1].Token;
+                string directionText = directionToken.Text.ToLower();
+            }
+            int u = 0;
+
+          
+        }
+    }
+}
---
+++ CameraDSLTutorial/CameraDSLTutorial/grammar/CameraControlGrammar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Irony.Parsing;
+
+namespace CameraDSLTutorial
+{
+    public class CameraControlGrammar : Grammar
+    {
+        public CameraControlGrammar() :
+            //#AS:2012/12/08: grammar is case-insensitive
+            base(caseSensitive: false)
+        {
+            var program = new NonTerminal("program");
+            var cameraSize = new NonTerminal("cameraSize");
+            var cameraPosition = new NonTerminal("cameraPosition");
+            var commandList = new NonTerminal("commandList");
+            var command = new NonTerminal("command");
+            var direction = new NonTerminal("direction");
+            var number = new NumberLiteral("number");
+            this.Root = program;
+            //Grammar production rules in bnf form
+            // <Program> ::= <CameraSize> <CameraPosition> <CommandList>
+            program.Rule = cameraSize + cameraPosition + commandList;
+
+            // <CameraSize> ::= "set" "camera" "size" ":" <number> "by" <number> "pixels" "."
+            cameraSize.Rule = ToTerm("set") + "camera" + "size" + ":" +
+                              number + "by" + number + "pixels" + ".";
+
+            // <CameraPosition> ::= "set" "camera" "position" ":" <number> "," <number> "."
+            cameraPosition.Rule = ToTerm("set") + "camera" + "position" +
+                                  ":" + number + "," + number + ".";
+
+            // <CommandList> ::= <Command>+
+            commandList.Rule = MakePlusRule(commandList, null, command);
+
+            // <Command> ::= "move" <number> "pixels" <Direction> "."
+            command.Rule = ToTerm("move") + number + "pixels" + direction + ".";
+
+            // <Direction> ::= "up" | "down" | "left" | "right"
+            direction.Rule = ToTerm("up") | "down" | "left" | "right";
+
+            //#AS:2012/12/08: these symbols are defined as puntuation, so they will not be included in the ast
+            this.MarkPunctuation("set", ("camera"), "size", ":", "by", "pixels", ".", "position", ",", "move");
+
+        }
+    }
+}
